f719334ed6f837104fbdafa03ad60584bbfd3419
Royal Garg <royalgarg36@gmail.com>
2024-02-06 16:41:27 +0530

    made components

---
+++ src/Homepage.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar.jsx';
+import Main from './components/Main.jsx';
+
+export default function Homepage () {
+
+    return (
+        <div className="homepage-wrapper">
+            <Navbar />
+            <Main />
+        </div>
+    );
+}
